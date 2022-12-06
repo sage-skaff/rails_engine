06f82cf2bb04f75cd0980f517aa3b17a9e2867a2
Sage Skaff <skaffsage@gmail.com>
2022-12-05 18:06:07 -0800

    Add items index endpoint and test

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'Items API' do
+  it 'can get a list of all items' do
+    create_list(:item, 3)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_an(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_an(Float)
+    end
+  end
+end
