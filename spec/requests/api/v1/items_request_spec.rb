0d9c0e662794d23be3f456e3bb33af5e0015f302
Sage Skaff <skaffsage@gmail.com>
2022-12-05 18:11:00 -0800

    Add item show endpoint and test

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -24,4 +24,26 @@ describe 'Items API' do
       expect(item[:attributes][:unit_price]).to be_an(Float)
     end
   end
+
+  it 'returns one item by its id' do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(String)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_an(String)
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_an(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_an(Float)
+  end
 end
