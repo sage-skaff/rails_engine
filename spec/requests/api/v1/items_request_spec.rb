f2d6b366cfc49e97ea1d57d7d1cf3f9bf8ce28e3
Sage Skaff <skaffsage@gmail.com>
2022-12-06 10:55:23 -0800

    Add test for item endpoint create new item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -46,4 +46,34 @@ describe 'Items API' do
     expect(item[:attributes]).to have_key(:unit_price)
     expect(item[:attributes][:unit_price]).to be_an(Float)
   end
+
+  it 'can create a new item' do
+    id = create(:merchant).id
+
+    item_params = {
+      name: Faker::Commerce.product_name,
+      description: Faker::Lorem.sentence,
+      unit_price: Faker::Commerce.price,
+      merchant_id: id
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+
+    post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(created_item).to have_key(:id)
+    expect(created_item[:id]).to be_an(String)
+
+    expect(created_item[:attributes]).to have_key(:name)
+    expect(created_item[:attributes][:name]).to be_an(String)
+
+    expect(created_item[:attributes]).to have_key(:description)
+    expect(created_item[:attributes][:description]).to be_an(String)
+
+    expect(created_item[:attributes]).to have_key(:unit_price)
+    expect(created_item[:attributes][:unit_price]).to be_an(Float)
+
+    expect(created_item[:attributes]).to have_key(:merchant_id)
+    expect(created_item[:attributes][:merchant_id]).to be_an(Integer)
+  end
 end
