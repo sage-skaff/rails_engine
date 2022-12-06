d6a4864afda89440a9f08e3902ba4a3e04d65e55
Sage Skaff <skaffsage@gmail.com>
2022-12-05 17:27:37 -0800

    Add merchants show endpoint and tests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -20,4 +20,20 @@ describe 'Merchants API' do
       expect(merchant[:attributes][:name]).to be_an(String)
     end
   end
+
+  it 'returns one merchant by their id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(String)
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_an(String)
+  end
 end
