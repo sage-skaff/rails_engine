b04911db7b54299b56d50b1adc6ef0a009402788
Sage Skaff <skaffsage@gmail.com>
2022-12-07 08:33:21 -0800

    Add validations to all models and tests

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,6 @@
 class Invoice < ApplicationRecord
+  validates_presence_of :customer_id, :status
+
   belongs_to :customer
   belongs_to :merchant
 
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,5 @@
 class Transaction < ApplicationRecord
+  validates_presence_of :invoice_id, :credit_card_number, :credit_card_expiration_date
+
   belongs_to :invoice
 end
