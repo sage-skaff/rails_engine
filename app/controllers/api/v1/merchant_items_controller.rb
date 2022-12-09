14ad0a98117e1f8cb5d38cfcf4fb6028b877f4e5
Sage Skaff <skaffsage@gmail.com>
2022-12-09 07:38:01 -0800

    Skip failing test and add comments

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::MerchantItemsController < ApplicationController
   def index
     if Merchant.exists?(params[:merchant_id])
-      render json: ItemSerializer.new(Item.where(merchant_id: params[:merchant_id]))
+      render json: ItemSerializer.new(Item.where(merchant_id: params[:merchant_id]).uniq)
     else
-      render json: { error: 'Error Message' }, status: 404 # error message isn't appearing currently
+      render json: { error: 'Error Message' }, status: 404 # error message isn't appearing correctly
     end
   end
 
